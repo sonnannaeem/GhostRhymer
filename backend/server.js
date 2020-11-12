28e3b1fca54d0d2b51aa76c8fbd41a7248a35fd5
Sonnan Naeem <sonnan.naeem@sjsu.edu>
2020-11-12 10:59:40 -0800

    Frontend init

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,17 @@ const express = require("express");//requires
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-// require('dotenv').config(); // config file -> sensitive information, hash or salt password, 
+require('dotenv').config(); // config file -> sensitive information, hash or salt password, 
 
 const app = express(); // opens the connection
 const port = 5000 //|| process.env.PORT; //typical backend port number, constant that we use in ENV file. In case our one port fails
 
-app.use(cors()); // web policy so you restrict the amount of resources from different ports (frontend/backend)
+app.use(cors()); // Web policy so you restrict the amount of resources from different ports (frontend/backend)
 app.use(express.json()); // calls a function that imports the express library of json so that we can read json objects through express
 
-//const MONGO_URI = process.env.MONGO_URI; //connection string to mongo on the backend
+const MONGO_URI = process.env.MONGO_URI; //connection string to mongo on the backend
 
-mongoose.connect('mongodb+srv://grhymer:ghostr@cluster0.rr0ll.mongodb.net/ghostrhymer?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=> {console.log("MONGO DB SUCCESSFULLY CONNECTED")})
     .catch((err) => console.log(err)); //flags for how connection should work
 
