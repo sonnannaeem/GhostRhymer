7cb84e9b1f14860e1771f0bda790584428d27de0
Shayan Asgari <shayan.asgari@sjsu.edu>
2020-11-14 17:44:26 -0800

    delete home and about links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,16 +13,7 @@ export default function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
+        
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
