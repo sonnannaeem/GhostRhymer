407f9dd2e4bf702f983b1f243feff93df17e6b04
Sonnan Naeem <sonnan.naeem@sjsu.edu>
2021-02-04 22:07:41 -0800

    Design layout start

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
+import "./About.css";
 
 export default function About() {
-    return (
-        <div>
-            <h2>About</h2>
+  return (
+    <div className="full-page-container">
+      <div className="shadow-container"></div>
+        <div className="text-container">
+            <input type="text" name="name" />
         </div>
-    )
+    </div>
+  );
 }
