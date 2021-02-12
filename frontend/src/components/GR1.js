b8af69acd33c77f3ebf72a634e4d82581b9e0406
amer mriziq <amer.mriziq@sjsu.edu>
2021-02-12 01:40:00 -0800

    Implemented Antd grid layout in GR1
    (not working completely yet though)

--- frontend/src/components/GR1.js
+++ frontend/src/components/GR1.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { Row, Col } from 'antd';
+import { Input } from 'antd';
+import { Layout } from 'antd';
+
 
 export default function GR1() {
-  return (
-    <div className="full-page-container">
 
-    </div>
+  const { TextArea } = Input;
+  const { Header, Footer, Sider, Content } = Layout;
+
+
+  return (
+    <Layout>
+      <Header>Header</Header>
+      <Layout>
+        <Sider><TextArea rows={4}/></Sider>
+        <Content>Content</Content>
+        <Sider>Sider</Sider>
+      </Layout>
+      <Footer>Footer</Footer>
+    </Layout>
   );
 }
