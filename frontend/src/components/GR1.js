e31fa205a1c446a77aaee5004c8b8d4d639ce450
Sonnan Naeem <sonnan.naeem@sjsu.edu>
2021-02-13 02:15:24 -0800

    Ant Des installation

--- frontend/src/components/GR1.js
+++ frontend/src/components/GR1.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { Row, Col } from 'antd';
-import { Input } from 'antd';
-import { Layout } from 'antd';
-
+import { DatePicker, Row, Col, Button } from "antd";
+import "antd/dist/antd.css";
 
 export default function GR1() {
-
   return (
-    <div></div>
+    <Row justify="center">
+      <Col span={8}>
+        <DatePicker />
+        <Button>PHAT</Button>
+      </Col>
+      <Col span={8}>
+        <DatePicker></DatePicker>
+        <Button>Bitch</Button>
+      </Col>
+      <Col span={8}>
+        <DatePicker></DatePicker>
+        <Button>Yo</Button>
+      </Col>
+    </Row>
   );
-
 }
